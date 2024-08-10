67b21bfcb5d323c4cc1c366ccbb5c57f9006ffa7
Darshan Sakpal <d.sakpal38@gmail.com>
2024-08-10 05:04:30 +0100

    Created the skeleton for the sidebar and grid for dashboard

--- penny-pal/src/app/app.module.ts
+++ penny-pal/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IncomeComponent } from './income/income.component';
+import { ExpenseComponent } from './expense/expense.component';
+import { LoginComponent } from './authentication/login/login.component';
+import { SignInComponent } from './authentication/sign-in/sign-in.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SidebarComponent } from './dashboard/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IncomeComponent,
+    ExpenseComponent,
+    LoginComponent,
+    SignInComponent,
+    DashboardComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
